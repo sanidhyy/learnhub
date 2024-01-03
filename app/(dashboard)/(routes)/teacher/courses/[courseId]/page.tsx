b4c58036d6cdca3361ee14c89b127217aa8a8f61
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-03 14:33:53 +0000

    course title update form implement

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -5,6 +5,8 @@ import { redirect } from "next/navigation";
 import { IconBadge } from "@/components/icon-badge";
 import { db } from "@/lib/db";
 
+import { TitleForm } from "./_components/title-form";
+
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const { userId } = auth();
 
@@ -44,11 +46,13 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
-        <div className="">
+        <div>
           <div className="flex items-center gap-x-2">
             <IconBadge icon={LayoutDashboard} />
             <h2 className="text-xl">Customize your course</h2>
           </div>
+
+          <TitleForm initialData={course} courseId={course.id} />
         </div>
       </div>
     </div>
