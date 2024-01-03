6f086031dcdfbed0d2f7065b09291193d9c33350
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-03 11:32:24 +0000

    logo height fix

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export const Logo = () => {
-  return <Image width={150} height={150} alt="logo" src="/logo.svg" />;
+  return <Image width={150} height={38} alt="logo" src="/logo.svg" />;
 };
