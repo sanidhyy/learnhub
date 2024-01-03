14e8bb6076e7938cd52194f77c7dfe4acae8ebe2
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-03 10:55:11 +0000

    sidebar implemented

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,10 +1,14 @@
 import type { PropsWithChildren } from "react";
 
+import { Navbar } from "./_components/navbar";
 import { Sidebar } from "./_components/sidebar";
 
 const DashboardLayout = ({ children }: PropsWithChildren) => {
   return (
     <div className="h-full">
+      <div className="h-[80px] md:pl-56 fixed inset-y-0 w-full z-50">
+        <Navbar />
+      </div>
       <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
         <Sidebar />
       </div>
