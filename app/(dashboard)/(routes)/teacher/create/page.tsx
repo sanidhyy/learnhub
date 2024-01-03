431ac5e6fdc89b4e5e26d37f2b5085417b0750da
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-03 11:37:06 +0000

    create course ui created

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -1,7 +1,113 @@
 "use client";
 
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import * as z from "zod";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+const formSchema = z.object({
+  title: z.string().min(1, {
+    message: "Title is required.",
+  }),
+});
+
 const CreatePage = () => {
-  return <div>CreatePage</div>;
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+    },
+  });
+
+  const { isSubmitting, isValid } = form.formState;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      const response = await axios.post("/api/course", values);
+
+      router.push(`/teacher/courses/${response.data?.id}`);
+    } catch {
+      toast.error("Something went wrong.");
+    }
+  };
+
+  return (
+    <div className="max-w-5xl mx-auto flex md:items-center md:justify-center h-full p-6">
+      <div className="">
+        <h1 className="text-2xl">Name your course</h1>
+
+        <p className="text-sm text-slate-600">
+          What would you like to name your course? Don&apos;t worry, you can
+          change this later.
+        </p>
+
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            autoComplete="off"
+            autoCapitalize="off"
+            className="space-y-8 mt-8"
+          >
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Course title</FormLabel>
+
+                  <FormControl>
+                    <Input
+                      placeholder="e.g. 'Advanced web development'"
+                      disabled={isSubmitting}
+                      aria-disabled={isSubmitting}
+                      {...field}
+                    />
+                  </FormControl>
+
+                  <FormDescription>
+                    What will you teach in this course?
+                  </FormDescription>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <div className="flex items-center gap-x-2">
+              <Button variant="ghost" type="button" asChild>
+                <Link href="/">Cancel</Link>
+              </Button>
+
+              <Button
+                type="submit"
+                disabled={!isValid || isSubmitting}
+                aria-disabled={!isValid || isSubmitting}
+              >
+                Continue
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
 };
 
 export default CreatePage;
