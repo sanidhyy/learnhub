de8babecef2f5c02a7a5d44ccaa839d1707d38e7
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-03 08:45:00 +0000

    css reset

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <Button>Click me</Button>
+      <Button size="lg" variant="destructive">
+        Click me
+      </Button>
     </div>
   );
 }
