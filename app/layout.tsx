1cca8d7f7f753a4bfc287fc62e77d455c38a54d6
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-03 16:32:07 +0000

    uploadthing ssr plugin added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,14 @@
 import { ClerkProvider } from "@clerk/nextjs";
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import type { PropsWithChildren } from "react";
+import { extractRouterConfig } from "uploadthing/server";
+
+import { appFileRouter } from "@/app/api/uploadthing/core";
+import { ToastProvider } from "@/components/providers/toaster-provider";
 
 import "./globals.css";
-import { ToastProvider } from "@/components/providers/toaster-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,9 +23,13 @@ export default function RootLayout({ children }: PropsWithChildren) {
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
+          {/* sonner toast */}
           <aside>
             <ToastProvider />
           </aside>
+
+          {/* uploadthing ssr plugin to prevent hydration errors */}
+          <NextSSRPlugin routerConfig={extractRouterConfig(appFileRouter)} />
 
           {children}
         </body>
