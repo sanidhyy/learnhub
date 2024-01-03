896cff4a634beefe47023037d3e4de0521f56779
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-03 16:31:17 +0000

    utfs.io hostname added to remote patterns

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      // uploadthing
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
