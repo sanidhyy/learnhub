4a9e248fcf789efce78fe60b4c3105b0b93b6f0b
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-05 14:50:54 +0000

    stripe and course payments implemented

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-enroll-button.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-enroll-button.tsx
@@ -1,3 +1,9 @@
+"use client";
+
+import axios from "axios";
+import { useState } from "react";
+import { toast } from "sonner";
+
 import { Button } from "@/components/ui/button";
 import { formatPrice } from "@/lib/format";
 
@@ -10,8 +16,29 @@ export const CourseEnrollButton = ({
   price,
   courseId,
 }: CourseEnrollButtonProps) => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onClick = async () => {
+    try {
+      setIsLoading(true);
+
+      const response = await axios.post(`/api/courses/${courseId}/checkout`);
+
+      window.location.assign(response.data.url);
+    } catch {
+      toast.error("Something went wrong.");
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <Button className="w-full md:w-auto" size="sm">
+    <Button
+      onClick={onClick}
+      disabled={isLoading}
+      aria-disabled={isLoading}
+      className="w-full md:w-auto"
+      size="sm"
+    >
       Enroll for {formatPrice(price)}
     </Button>
   );
