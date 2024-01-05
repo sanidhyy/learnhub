8be7cc166ef0d98a94a0899c197639d255a42570
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-05 06:27:33 +0000

    categories query implemented

---
+++ app/(dashboard)/(routes)/search/_components/categories.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import type { Category } from "@prisma/client";
+import type { IconType } from "react-icons";
+import {
+  FcEngineering,
+  FcFilmReel,
+  FcMultipleDevices,
+  FcMusic,
+  FcOldTimeCamera,
+  FcSalesPerformance,
+  FcSportsMode,
+} from "react-icons/fc";
+
+import { CategoryItem } from "./category-item";
+
+type CategoriesProps = {
+  items: Category[];
+};
+
+const iconMap: Record<Category["name"], IconType> = {
+  Music: FcMusic,
+  Photography: FcOldTimeCamera,
+  Fitness: FcSportsMode,
+  Accounting: FcSalesPerformance,
+  "Computer Science": FcMultipleDevices,
+  Filming: FcFilmReel,
+  Engineering: FcEngineering,
+};
+
+export const Categories = ({ items }: CategoriesProps) => {
+  return (
+    <div className="flex items-center gap-x-2 overflow-x-auto pb-2">
+      {items.map((item) => (
+        <CategoryItem
+          key={item.id}
+          label={item.name}
+          icon={iconMap[item.name]}
+          value={item.id}
+        />
+      ))}
+    </div>
+  );
+};
