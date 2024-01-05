b3db452c0b63a5908c10559e0ec70787b0b2367a
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-05 06:02:57 +0000

    courses table ui implemented

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,13 +1,28 @@
-import Link from "next/link";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-import { Button } from "@/components/ui/button";
+import { db } from "@/lib/db";
 
-const CoursesPage = () => {
+import { columns } from "./_components/columns";
+import { DataTable } from "./_components/data-table";
+
+const CoursesPage = async () => {
+  const { userId } = auth();
+
+  if (!userId) return redirect("/");
+
+  const courses = await db.course.findMany({
+    where: {
+      userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
   return (
     <div className="p-6">
-      <Button>
-        <Link href="/teacher/create">New Course</Link>
-      </Button>
+      <DataTable columns={columns} data={courses} />
     </div>
   );
 };
