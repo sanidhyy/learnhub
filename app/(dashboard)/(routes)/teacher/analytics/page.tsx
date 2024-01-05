c88444ead4fa05e62eb938867014bde3f02206d8
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-05 16:28:13 +0000

    analytics page done

--- app/(dashboard)/(routes)/teacher/analytics/page.tsx
+++ app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -1,5 +1,28 @@
-const AnalyticsPage = () => {
-  return <div>AnalyticsPage</div>;
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import { getAnalytics } from "@/actions/get-analytics";
+
+import { Chart } from "./_components/chart";
+import { DataCard } from "./_components/data-card";
+
+const AnalyticsPage = async () => {
+  const { userId } = auth();
+
+  if (!userId) return redirect("/");
+
+  const { data, totalRevenue, totalSales } = await getAnalytics(userId);
+
+  return (
+    <div className="p-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+        <DataCard label="Total Revenue" value={totalRevenue} shouldFormat />
+        <DataCard label="Total Sales" value={totalSales} />
+      </div>
+
+      <Chart data={data} />
+    </div>
+  );
 };
 
 export default AnalyticsPage;
