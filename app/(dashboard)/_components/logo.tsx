15e6f610ce01aaf5ae4ba8921cda68c2f1bd2de1
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-05 06:11:34 +0000

    link added to logo

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export const Logo = () => {
-  return <Image width={150} height={38} alt="logo" src="/logo.svg" />;
+  return (
+    <Link href="/">
+      <Image width={150} height={38} alt="logo" src="/logo.svg" />
+    </Link>
+  );
 };
