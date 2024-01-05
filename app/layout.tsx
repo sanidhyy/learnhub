97f10aab74dba8852da8b2212c34b89ba3855534
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-05 17:19:40 +0000

    site config updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import type { PropsWithChildren } from "react";
 import { extractRouterConfig } from "uploadthing/server";
@@ -8,16 +8,17 @@ import { extractRouterConfig } from "uploadthing/server";
 import { appFileRouter } from "@/app/api/uploadthing/core";
 import { ConfettiProvider } from "@/components/providers/confetti-provider";
 import { ToastProvider } from "@/components/providers/toaster-provider";
+import { siteConfig } from "@/config";
 
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "LearnHub",
-  description:
-    "Discover seamless learning with LearnHub - your all-in-one Learning Management System.",
+export const viewport: Viewport = {
+  themeColor: "#007DFC",
 };
+
+export const metadata: Metadata = siteConfig;
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
--- config/index.ts
+++ config/index.ts
@@ -1,6 +1,50 @@
 import type { Metadata } from "next";
 
-export const siteConfig: Metadata = {} as const;
+export const siteConfig: Metadata = {
+  title: "LearnHub",
+  description:
+    "Discover seamless learning with LearnHub - your all-in-one Learning Management System.",
+  keywords: [
+    "reactjs",
+    "nextjs",
+    "vercel",
+    "react",
+    "learnhub",
+    "learning management",
+    "shadcn",
+    "shadcn-ui",
+    "radix-ui",
+    "cn",
+    "clsx",
+    "platform",
+    "recharts",
+    "sonner",
+    "mux",
+    "sql",
+    "mysql",
+    "aiven",
+    "prisma",
+    "lucide-react",
+    "react-icons",
+    "next-themes",
+    "postcss",
+    "prettier",
+    "react-dom",
+    "tailwindcss",
+    "tailwindcss-animate",
+    "ui/ux",
+    "js",
+    "javascript",
+    "typescript",
+    "eslint",
+    "html",
+    "css",
+  ] as Array<string>,
+  authors: {
+    name: "Sanidhya Kumar Verma",
+    url: "https://github.com/sanidhyy",
+  },
+} as const;
 
 export const links = {
   sourceCode: "https://github.com/sanidhyy/learnhub",
