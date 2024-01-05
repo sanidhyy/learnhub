d90cb3e88054e59884f0302cf0423e3de82c6eae
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-05 17:07:52 +0000

    site config initialised

---
+++ config/index.ts
@@ -0,0 +1,7 @@
+import type { Metadata } from "next";
+
+export const siteConfig: Metadata = {} as const;
+
+export const links = {
+  sourceCode: "https://github.com/sanidhyy/learnhub",
+} as const;
